9b82de99d2f16affc658157fd89596820e5c6d9d
shaheen54 <Abrar@LAPTOP-989OEA83.shaheen>
2022-01-20 19:48:25 -0500

    first commit

---
+++ src/main/java/com/automation/Animaljava.java
@@ -0,0 +1,21 @@
+package com.automation;
+
+public class Animaljava {
+public void getduplicate(int[] myArray) {
+	  
+	
+	for(int i=0;i<myArray.length;i++) {
+		}
+	}
+       
+
+	
+	   
+   
+ 
+ public String money(){
+	 
+	 String myname="bobby";
+	 return myname;
+ }
+}
---
+++ src/test/java/com/practice/TestRun.java
@@ -0,0 +1,24 @@
+package com.practice;
+
+
+public class TestRun {
+	
+	
+	public void getsalary(int a, int b) {
+		System.out.println("before swap="+a);
+		System.out.println("before swap="+b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("after swap="+a);
+		System.out.println("after swap="+b);
+		
+	
+	  
+		
+	}
+	
+}
+
+
+	
